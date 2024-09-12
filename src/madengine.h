b139a095ace2532eb582908b6c88a1dc4c67713c
Benjamin-Braun <ben.braun@posteo.de>
2024-09-12 17:46:47 +0200

    project setup

---
+++ src/madengine.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#ifdef __APPLE__
+    #define GL_SILENCE_DEPRECATION
+    #include <OpenGL/gl.h>
+    #include <OpenGL/glu.h>
+    #include <GL/glut.h>
+#else
+    #include <GL/glut.h>
+#endif
+#include <math.h>
+
+using namespace std;
+
+#include "renderer.h"
